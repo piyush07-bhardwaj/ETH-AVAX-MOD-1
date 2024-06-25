e8eab6ed4f0b99d0d5ff15a1babf7959cb1038c6
piyush07-bhardwaj <146122258+piyush07-bhardwaj@users.noreply.github.com>
2024-06-25 10:39:12 +0530

    Create Module1.sol

---
+++ Module1.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Library {
+    address public admin;
+
+    uint bookPrice;
+    uint bookQuantity;
+
+    mapping (address => uint) userBalance; 
+
+    constructo
+     bookQuantity = _bookQuantity;
+        bookPrice = _bookPrice;
+
+        admin = msg.sender;
+    }
