1353be35a4d2fd26b5d80b0eba74e0ae3e7fe7b9
piyush07-bhardwaj <146122258+piyush07-bhardwaj@users.noreply.github.com>
2024-06-25 10:42:46 +0530

    Update Module1.sol

--- Module1.sol
+++ Module1.sol
@@ -9,9 +9,57 @@ contract Library {
 
     mapping (address => uint) userBalance; 
 
-    constructo
+    constructor(uint _bookPrice, uint _bookQuantity) {
      bookQuantity = _bookQuantity;
         bookPrice = _bookPrice;
 
         admin = msg.sender;
     }
+
+// Modifiers
+
+    modifier isAdmin {
+        require(admin == msg.sender, "You don't have access!!");
+        _;
+    }
+
+    modifier hasMoney(address user) {
+        if(userBalance[user] < bookPrice) {
+            revert("You don't have suffienct balance to buy the book!!");
+        }
+        _;
+    }
+
+// Functions
+
+    function changePrice(uint newPrice) public isAdmin returns(string memory) {
+        bookPrice = newPrice;
+
+        assert(admin == msg.sender);
+        assert(bookPrice == newPrice);
+        return "Book Price Changed!!";
+    }
+function addBook() public isAdmin returns(string memory) {
+        bookQuantity += 1;
+
+        assert(admin == msg.sender);
+        return "Book Quantity Increased!!";
+    }
+
+    function addMoney(address user, uint amount) public returns(string memory) {
+        userBalance[user] += amount;
+
+        assert(userBalance[user] >= amount);
+        return "User balance increased!!";
+    }
+
+    function buyBook(address user) public hasMoney(user) returns(string memory) {
+        bookQuantity -= 1;
+        userBalance[user] -= bookPrice;
+
+        return "Book Buying successful!!";
+    }
+    
+}
+      
+      
